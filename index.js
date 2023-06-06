cccf87527809c64aad6ae310f3f3ae6fcbcd5c57
Sankoukai <supersankoukai@gmail.com>
2023-06-07 01:35:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
        		 data: {
           		content: `Nombres de membres sur SFF: ${response.data.approximate_member_count}`,	
         		},
-		flags: 6
+		flags: `1 << 6`,
       		});
 	}
 	    catch(e){
