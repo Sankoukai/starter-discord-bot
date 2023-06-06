ad313296e90436fa8ff6435693ad5515df8d0b5b
Sankoukai <supersankoukai@gmail.com>
2023-06-06 22:07:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `Yo ${interaction.members}!`,
+          content: `Yo ${interaction.member}!`,
         },
       });
     }
