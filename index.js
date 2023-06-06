13a731acf6a0ac5dfcb803e4edc04acfcf5b0ba1
Sankoukai <supersankoukai@gmail.com>
2023-06-06 22:03:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ const discord_api = axios.create({
 
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
-	
+	//.members.filter((member) => !member.user.bot).size
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
     console.log(interaction.data.name)
     if(interaction.data.name == 'test'){
       return res.send({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
         data: {
-          content: `Yo ${interaction.guild.members.filter((member) => !member.user.bot).size}!`,
+          content: `Yo ${interaction.guild}!`,
         },
       });
     }
