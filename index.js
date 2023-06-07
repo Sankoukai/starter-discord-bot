96a8cc896282addaa4c1cb1f087cc991faf73cd3
Sankoukai <supersankoukai@gmail.com>
2023-06-07 17:16:47 +0200

    ezr

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const APPLICATION_ID = process.env.APPLICATION_ID
 const TOKEN = process.env.TOKEN 
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
 const GUILD_ID = process.env.GUILD_ID 
-
+const express = require('express');
+const app = express();
 const {Client,Partials, Collection} = require('discord.js');
 const {User, Message, GuildMember, ThreadMembers} = Partials
 const client = new Client({
@@ -20,8 +21,18 @@ console.log(`const loadEvents !!`);
 const { loadEvents } = require("./Handlers/eventHandler");
 console.log(`const loadCommands !!`);
 const { loadCommands } = require('./Handlers/commandHandler');
-console.log(`const loadEvents !!`);
-loadEvents(client);
-console.log(`client  ${client} client !!`);
+
 
 client.login(TOKEN);
+app.get('/launch', async (req,res) =>{
+  try{
+    console.log(`const loadEvents !!`);
+    loadEvents(client);
+    console.log(`client  ${client} client !!`);
+    return res.send('commands have been registered')
+  }catch(e){
+    console.error(e.code)
+    console.error(e.response?.data)
+    return res.send(`${e.code} error from discord`)
+  }
+})
