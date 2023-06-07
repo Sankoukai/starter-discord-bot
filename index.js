339957b8df73f35bcee2575c1a3851138c6e3c7d
Sankoukai <supersankoukai@gmail.com>
2023-06-07 16:53:51 +0200

    launch

--- index.js
+++ index.js
@@ -9,18 +9,24 @@ const client = new Client({
     intents:[3276799],
     partials:[User, Message, GuildMember, ThreadMembers]
 });
-
+const express = require('express');
+const app = express();
 client.commands = new Collection();
 client.subCommands = new Collection();
 client.events = new Collection();
 client.memberGuildConfig = new Collection();
 client.messageGuildConfig = new Collection();
 
-   console.log(`const loadEvents !!`);
+console.log(`const loadEvents !!`);
 const { loadEvents } = require("./Handlers/eventHandler");
 console.log(`const loadCommands !!`);
 const { loadCommands } = require('./Handlers/commandHandler');
 console.log(`const loadEvents !!`);
 loadEvents(client);
 console.log(`client  ${client} client !!`);
+
+
+app.get('/launch', async (req,res) =>{
 client.login(TOKEN);
+return res.send('launch ok')
+}
