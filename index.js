e43fe34e3c2a7e12340695c2fff7117fedb971c1
Sankoukai <supersankoukai@gmail.com>
2023-06-07 13:41:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
     }
  if(interaction.data.name == 'cammy'){
 	try{
-	   let guild = await discord_api.get(`/guilds/${GUILD_ID}`);
 	   let response = (await discord_api.get(`/guilds/${GUILD_ID}/members?limit=2`))
 	   	console.log(`${util.inspect(response.data)}`);
       		return res.send({
@@ -58,8 +57,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
        		 data: {
           		content: `Nombre de Cammy sur SFF: ${response.data.filter(
 				(member) => {
-					console.log(`${util.inspect(guild.data)}`);
-					return member.roles.includes('1105860664624418836');
+					return member.roles.includes('1105040765186474015');
 				})
 			 .map(
 				(member) => `@${member.user.display_name}`
@@ -72,6 +70,279 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
 		  console.log(`MY ERROR ${e}`)
     	}
     }
+	  /*
+	  {
+      id: '1105035482582233160',
+      name: '🏝️ Deejay',
+      description: null,
+      permissions: 0,
+      position: 56,
+      color: 3066993,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105035703412330506',
+      name: '🎌 Honda',
+      description: null,
+      permissions: 0,
+      position: 55,
+      color: 3447003,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 0,
+      permissions_new: '0'
+    },
+    {
+      id: '1105035782735011902',
+      name: '🥋 Ryu',
+      description: null,
+      permissions: 0,
+      position: 54,
+      color: 16777215,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105035997705682974',
+      name: '🔥 Ken',
+      description: null,
+      permissions: 0,
+      position: 53,
+      color: 12648448,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105036157554802688',
+      name: '🍔 Guile',
+      description: null,
+      permissions: 0,
+      position: 52,
+      color: 4880154,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105036320394457178',
+      name: '🕉️ Dhalsim',
+      description: null,
+      permissions: 0,
+      position: 51,
+      color: 11027200,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105037949990617129',
+      name: '👘 Chunli',
+      description: null,
+      permissions: 0,
+      position: 50,
+      color: 2662853,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039025779261440',
+      name: '💪 Marisa',
+      description: null,
+      permissions: 0,
+      position: 49,
+      color: 16773120,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039198588776448',
+      name: '🩰 Manon',
+      description: null,
+      permissions: 0,
+      position: 48,
+      color: 7716847,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039328834502739',
+      name: '🍺 Jamie',
+      description: null,
+      permissions: 0,
+      position: 47,
+      color: 16753152,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039593235042355',
+      name: '🎶 Kimberly',
+      description: null,
+      permissions: 0,
+      position: 46,
+      color: 14096640,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039726815232020',
+      name: '🥊 Luke',
+      description: null,
+      permissions: 0,
+      position: 45,
+      color: 16086,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039877797580942',
+      name: '🦧 Blanka',
+      description: null,
+      permissions: 0,
+      position: 44,
+      color: 8448527,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      
+2023-06-07 13:37:30.278: unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105039968210001972',
+      name: '👣 Juri',
+      description: null,
+      permissions: 0,
+      position: 43,
+      color: 11534568,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105040328429420554',
+      name: '🦯 JP',
+      description: null,
+      permissions: 0,
+      position: 42,
+      color: 5905291,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105040765186474015',
+      name: '🍵 Cammy',
+      description: null,
+      permissions: 0,
+      position: 41,
+      color: 45823,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105040908291952650',
+      name: '🏏Lily',
+      description: null,
+      permissions: 0,
+      position: 40,
+      color: 10737238,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    },
+    {
+      id: '1105041405753167922',
+      name: '🐻 Zangief',
+      description: null,
+      permissions: 0,
+      position: 39,
+      color: 15073280,
+      hoist: false,
+      managed: false,
+      mentionable: true,
+      icon: null,
+      unicode_emoji: null,
+      flags: 1,
+      permissions_new: '0'
+    }
+	  */
 
     /*if(interaction.data.name == 'dm'){
       // https://discord.com/developers/docs/resources/user#create-dm
