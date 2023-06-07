a0b734873d84c6700007ce53ee050611b8ed2cc2
Sankoukai <supersankoukai@gmail.com>
2023-06-07 13:02:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
           		content: `Nombre de Cammy sur SFF: ${response.data.filter(
 				(member) => {
 					console.log(`${member.roles.includes('🍵 Cammy')} -- ${member.roles.includes('🍵 cammy')} -- ${member.roles.includes('🍵Cammy')}`);
-					member.roles.includes('🍵cammy')
+					member.roles.includes('1105860664624418836')
 				})
 			 .map(
 				(member) => `@${member.user.username}`
