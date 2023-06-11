bb338b804ec37d4f5b40148b7786c3ec25bd69df
Sankoukai <supersankoukai@gmail.com>
2023-06-11 21:22:58 +0200

    zer

--- index.js
+++ index.js
@@ -74,8 +74,8 @@ async function sendMessageForSpecificRole(res,id){
 }
 
 async function tournamentList(res,tournament){
-      let response = (await challonge_api.get(`/tournaments.json`))
-      console.log(`ALORS ? ${util.inspect(response)}`)
+      let response = (await challonge_api.get(`/tournaments.json?api_key=${CHALLONGE_API_KEY}`))
+      console.log(`ALORS ? ${util.inspect(response.data)}`)
           return res.send({
               type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
               data: {
