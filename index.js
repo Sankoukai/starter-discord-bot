1aa6af794b1503ea07a06e0e3070d77b1f8c674a
Sankoukai <supersankoukai@gmail.com>
2023-06-11 19:38:24 +0200

    flatten

--- index.js
+++ index.js
@@ -62,6 +62,16 @@ async function sendMessageForSpecificRole(res,id){
       }
 }
 
+function flatten(array)
+{
+    if(array.length == 0)
+        return array;
+    else if(Array.isArray(array[0]))
+        return flatten(array[0]).concat(flatten(array.slice(1)));
+    else
+        return [array[0]].concat(flatten(array.slice(1)));
+}
+
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
   if (interaction.type === InteractionType.APPLICATION_COMMAND) {
@@ -358,7 +368,7 @@ app.get('/register_commands', async (req,res) => {
       },]
     )
   
-  slash_commands.push(tournamentsCommands.flat(1))
+  slash_commands.push(flatten(tournamentsCommands))
 
   try
   {
