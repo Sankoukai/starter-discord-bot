a85160ca18c0c96e7d75bf7ba9da02a97b42c354
Sankoukai <supersankoukai@gmail.com>
2023-06-16 00:55:33 +0200

    ,n,nb,n

--- index.js
+++ index.js
@@ -66,13 +66,17 @@ axios.request({
   console.log(`ALORS ? ${response}`);
 });*/
 
-var bodyFormData = new FormData();
+/*var bodyFormData = new FormData();
 bodyFormData.append('client_id', CHALLONGE_CLIENT_ID);
 bodyFormData.append('scope', 'me%20tournaments:read%20matches:read%20participants:read');
 bodyFormData.append('response_type', 'code');
 bodyFormData.append('client_id', "https://oauth.pstmn.io/v1/callback");
 // curl --location 'https://api.challonge.com/oauth/authorize?scope=me%20tournaments%3Aread%20matches%3Aread%20participants%3Aread&client_id=2b1b111ae6ac0fbd98c1207fd3a066cfa839c2854c11f8dd1cdec40ffbef1818&redirect_uri=https%3A%2F%2Foauth.pstmn.io%2Fv1%2Fcallback&response_type=code'
 challonge_oauth_api.get(`/oauth/authorize?scope=me tournaments:read participants:write&client_id=${CHALLONGE_CLIENT_ID}&redirect_uri=https://oauth.pstmn.io/v1/callback&response_type=code`).then(response => {
+  console.log(`ALORS ? ${util.inspect(response.data)}`);
+});*/
+
+challonge_api.get(`/v2/tournaments.json`).then(response => {
   console.log(`ALORS ? ${util.inspect(response.data)}`);
 });
 
