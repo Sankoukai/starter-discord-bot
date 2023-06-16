dd98c5326ce8e80fb007795c2392b64edeea7d9d
Sankoukai <supersankoukai@gmail.com>
2023-06-16 02:18:06 +0200

    dfgdfg

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
               }
               ).then(response => {
                   console.log(`ALORS ? ${util.inspect(response.data)}`)
-          }));
+          });
 
         let response = (await discord_api.get(`/guilds/${GUILD_ID}?with_counts=true`))
           return res.send({
